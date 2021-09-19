12bf51005b6c422e68cad6d0196c4712c6697ff7
yousofh <yousofh100@gmail.com>
2021-09-19 22:30:59 +0430

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'neat tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this beautiful tree today'
+    },
+    {
+      title: 'Snowy mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'I saw this beautiful snowy mountain today'
+    },
+    {
+      title: 'Mountain Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biking22',
+      content: 'I did some biking today'
+    },
+    {
+      title: 'Mountain Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biking22',
+      content: 'I did some biking today'
+    }
+  ]
 }
